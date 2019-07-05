00850affec7326631c4b7170a08c4a4ce9092fa4
Rafael Teck Gomes <rafaelteckgomes@MacBook-Pro-de-Rafael.local>
2019-07-04 22:04:42 -0300

    update project

--- nodejs-project/config/database.js
+++ nodejs-project/config/database.js
@@ -10,7 +10,7 @@ mongoose.connect(
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  // Database connected!!!
   console.log("Connected to MongoDB")
 });
 
